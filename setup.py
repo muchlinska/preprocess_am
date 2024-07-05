72fe89eb3bf2eeb1118b8dd343f7a0902af2e59d
muchlinska <anna.muchlisnka@gmail.com>
2024-07-05 09:21:37 +0200

    python package for https://bit.ly/intro_nlp

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('READ ME.md', 'r') as file:
+	long_description = file.read()
+
+
+setuptools.setup(
+	name = 'preprocess_am',
+	version = '0.0.1',
+	author = 'Anna M',
+	author_email = 'anna.muchlinska@gmail.com',
+	description = 'This is preprocessing package',
+	long_description = 'long_description',
+	long_description_content_type = 'text/markdown',
+	packages = setuptools.find_packages(),
+	classifiers = [
+	'Progmramming Language :: Python :: 3',
+	'License :: OSI Aproved :: MIT License',
+	'Operating System :: OS Independent'],
+	python_requires = '>=3.12'
+	)
